478f472b9f8c6c3a98097e3a76e8a7e6065b0c74
Ivan Ustyantsev <vangold915@gmail.com>
2024-06-27 23:55:05 +0700

    React-router-dom (preloading data via routing): refactoring BrowserRouter, create routerwith createBrowserRouter helper

--- MN/react-router-dom-v6/src/App.jsx
+++ MN/react-router-dom-v6/src/App.jsx
@@ -1,74 +1,19 @@
 import "./App.css"
-import { Navigate, Route, Routes } from "react-router-dom"
-import { AboutPage } from "./pages/AboutPage"
-import { NotFoundPage } from "./pages/NotFoundPage"
-import { Layout } from "./components/Layout"
-import { PostPage } from "./pages/PostPage"
-import { HomePage } from "./pages/HomePage"
-import { CreatePostPage } from "./pages/CreatePostPage"
-import { EditPostPage } from "./pages/EditPostPage"
-import { LoginPage } from "./pages/LoginPage"
-import { RequireAuth } from "./hoc/RequireAuth"
+import {
+  RouterProvider,
+} from "react-router-dom"
 import { AuthProvider } from "./hoc/AuthProvider"
-import { BlogPage } from "./pages/BlogPage"
-import { SearchPage } from "./pages/SearchPage"
-import { PostPageForSearch } from "./pages/PostPageForSearch"
-import { EditPostPageForSearch } from "./pages/EditPostPageForSearch"
-import { Contacts } from "./components/Contacts"
-import { Team } from "./components/Team"
+import { router } from "./router/router"
+
+
+
 
 const App = () => {
   return (
     <div className="App">
-      <AuthProvider>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<HomePage />} />
-            <Route path="posts/:id" element={<PostPage />} />
-            <Route
-              path="posts/:id/edit"
-              element={
-                <RequireAuth>
-                  <EditPostPage />
-                </RequireAuth>
-              }
-            />
-            <Route path="posts/:_page?/:_limit?" element={<BlogPage />} />
-            <Route
-              path="posts/create"
-              element={
-                <RequireAuth>
-                  <CreatePostPage />
-                </RequireAuth>
-              }
-            />
-            <Route path="posts/create/edit" element={<NotFoundPage />} />
-            <Route path="about-us/*" element={<AboutPage />}>
-              <Route path="contacts" element={<Contacts />} />
-              <Route path="team" element={<Team />} />
-            </Route>
-            <Route
-              path="about/*"
-              element={<Navigate to={"/about-us/*"} replace />}
-            ></Route>
-            <Route path="search-posts/:id" element={<PostPageForSearch />} />
-            <Route
-              path="search-posts/:id/edit"
-              element={
-                <RequireAuth>
-                  <EditPostPageForSearch />
-                </RequireAuth>
-              }
-            />
-            <Route
-              path="search-posts/:_page?/:_limit?/:search?"
-              element={<SearchPage />}
-            />
-            <Route path="login" element={<LoginPage />} />
-            <Route path="*" element={<NotFoundPage />} />
-          </Route>
-        </Routes>
-      </AuthProvider>
+        <AuthProvider>
+          <RouterProvider router={router}/>
+        </AuthProvider>
     </div>
   )
 }
---
+++ MN/react-router-dom-v6/src/router/router.js
@@ -0,0 +1,72 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Navigate,
+  Route
+} from "react-router-dom"
+import { AboutPage } from "../pages/AboutPage"
+import { NotFoundPage } from "../pages/NotFoundPage"
+import { Layout } from "../components/Layout"
+import { PostPage } from "../pages/PostPage"
+import { HomePage } from "../pages/HomePage"
+import { CreatePostPage } from "../pages/CreatePostPage"
+import { EditPostPage } from "../pages/EditPostPage"
+import { LoginPage } from "../pages/LoginPage"
+import { RequireAuth } from "../hoc/RequireAuth"
+import { BlogPage } from "../pages/BlogPage"
+import { SearchPage } from "../pages/SearchPage"
+import { PostPageForSearch } from "../pages/PostPageForSearch"
+import { EditPostPageForSearch } from "../pages/EditPostPageForSearch"
+import { Contacts } from "../components/Contacts"
+import { Team } from "../components/Team"
+
+
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<HomePage />} />
+      <Route path="posts/:id" element={<PostPage />} />
+      <Route
+        path="posts/:id/edit"
+        element={
+          <RequireAuth>
+            <EditPostPage />
+          </RequireAuth>
+        }
+      />
+      <Route path="posts/:_page?/:_limit?" element={<BlogPage />} />
+      <Route
+        path="posts/create"
+        element={
+          <RequireAuth>
+            <CreatePostPage />
+          </RequireAuth>
+        }
+      />
+      <Route path="posts/create/edit" element={<NotFoundPage />} />
+      <Route path="about-us/*" element={<AboutPage />}>
+        <Route path="contacts" element={<Contacts />} />
+        <Route path="team" element={<Team />} />
+      </Route>
+      <Route
+        path="about/*"
+        element={<Navigate to={"/about-us/*"} replace />}
+      ></Route>
+      <Route path="search-posts/:id" element={<PostPageForSearch />} />
+      <Route
+        path="search-posts/:id/edit"
+        element={
+          <RequireAuth>
+            <EditPostPageForSearch />
+          </RequireAuth>
+        }
+      />
+      <Route
+        path="search-posts/:_page?/:_limit?/:search?"
+        element={<SearchPage />}
+      />
+      <Route path="login" element={<LoginPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Route>
+  )
+)
